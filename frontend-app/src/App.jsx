030a7e15f59256935c745528dcc327dc86c329ed
0147.19 <txt0147.03@gmail.com>
2026-01-17 00:11:28 +0700

    Update App.jsx

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -12,7 +12,7 @@ import {
 } from 'lucide-react';
 import './App.css';
 
-const API_URL = "https://emomap6-be.onrender.com/api";
+const API_URL = "https://racial-nested-restore-asset.trycloudflare.com/api";
 // --- COMPONENT 2: CHAT INTERFACE (Giao diện Chat - Đã sửa lỗi & Thêm Icon) ---
 const ChatInterface = () => {
   const navigate = useNavigate();
